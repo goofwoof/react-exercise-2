5a232f693ba157795e9749f0bf5e9de387ab4161
goofwoof <heli0086@outlook.com>
2020-08-11 22:04:05 +0800

    linshi 提交

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { getDefaultNormalizer } from '@testing-library/react';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data = getData(),
+      shopCount = 0
+    }
+    
+    this.giveUp = ()=>{
+      this.setState({
+        ...this.state, 
+        shopCount: this.state.shopCount -1 
+      })
+    }
+
+    this.buy = ()=>{
+      this.setState({
+        ...this.state, 
+        shopCount: this.state.shopCount +1
+      })
+    }
+  }
+  getData = ()=>{
+    url = 
+    let data = fetch(url, {
+      headers: { 'content-type': 'application/json' },
+      method: 'Get'
+    }).then(response => response.json());
+  }
+
   render() {
     return (
-      <main className="app">
-      </main>
+      <header className="head">
+        <ShopCar id="shopCar" info={this.state.shopCount}/>
+      </header>
     );
   }
 }
